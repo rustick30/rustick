a0ce4da044c25626829a8dd4428cb1b8c0355ff8
rustick30 <72869024+rustick30@users.noreply.github.com>
2020-10-20 14:14:03 +0300

    sd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner in=new Scanner(System.in);
+        double a;
+        a=in.nextDouble(a);
+        System.out.println(a%10);
     }
 }
