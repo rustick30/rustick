468856a5ea5157fd250a9af5f2a9e3e7de1a30f7
rustick30 <72869024+rustick30@users.noreply.github.com>
2020-10-20 18:30:42 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner in=new Scanner(System.in);
-        double a;
-        a=in.nextDouble(a);
-        System.out.println(a%10);
+        int a [][]=new int [3][4];
+        int b [][]=new int [3][4];
+
+        Scanner sc=new Scanner(System.in);
+
+        for (int i=0;i<3;i++) {
+            for (int j = 0; j < 4; j++) {
+                a[i][j]=sc.next().charAt(j);
+            }
+        }
+        for(int i=0;i<3;i++){
+            for(int j=0)
+        }
+        System.out.println(a);
     }
 }
